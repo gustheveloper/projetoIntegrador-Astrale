ccf06e3fab7a6976ddf206c7f340c8b7c82c41fc
Gustavo Corrêa <guts.correa@gmail.com>
2019-10-20 18:15:08 -0300

    adicionei cards para os posts do blog + página do post, de fato

---
+++ blog.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/StyleGus.css" rel="stylesheet">
+    <title>Header</title>
+    <?php
+    include_once('header.html')
+     ?>
+     <main>
+       <section>
+           <div class="blogArea">
+             <p>Blog</p>
+
+           </div>
+       </section>
+       <section>
+         <div class="posts">
+           <div class="row">
+             <div class="card">
+               <img src="image\1200x1200_PI_horoscope._SX970._CB488496935_._SX970_.jpg" style="height: 60vh" alt="">
+               <div class="card-body">
+                 <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h5>
+                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 <a href="página-blog.php"><button type="button" class="btn btn-outline-info">SAIBA MAIS</button></a>
+               </div>
+             </div>
+           </div>
+           <div class="row">
+             <div class="card">
+               <img src="image\1200x1200_PI_horoscope._SX970._CB488496935_._SX970_.jpg" style="height: 60vh" alt="">
+               <div class="card-body">
+                 <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h5>
+                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 <a href="página-blog.php"><button type="button" class="btn btn-outline-info">SAIBA MAIS</button></a>
+               </div>
+             </div>
+           </div>
+           <div class="row">
+             <div class="card">
+               <img src="image\1200x1200_PI_horoscope._SX970._CB488496935_._SX970_.jpg" style="height: 60vh" alt="">
+               <div class="card-body">
+                 <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h5>
+                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 <a href="página-blog.php"><button type="button" class="btn btn-outline-info">SAIBA MAIS</button></a>
+               </div>
+             </div>
+           </div>
+
+         </div>
+       </section>
+     </main>
+     <footer class="d-flex justify-content-center align-items-center">
+             © Coryright Group Astrale
+     </footer>
+ </body>
+ </html>
---
+++ página-blog.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/StyleGus.css" rel="stylesheet">
+    <title>Header</title>
+    <?php
+    include_once('header.html')
+     ?>
+     <main>
+       <section>
+           <div class="blogArea">
+             <p>Blog</p>
+
+           </div>
+       <section class="text-post">
+         <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h3>
+       </section>
+       <article class="text-post">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+         </article>
+         <div class="d-flex justify-content-center p-3">
+           <img src="image\professional-astrologer-update.jpg" alt="">
+         </div>
+         <article class="text-post">
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       </article>
+     </main>
+     <footer class="d-flex justify-content-center align-items-center">
+             © Coryright Group Astrale
+     </footer>
+   </body>
+   </html>
